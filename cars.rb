1127122b0708528340a7705ce9a72825bc44e2a2
Shehzan Devani <shehzandevani@gmail.com>
2013-09-03 13:57:55 -0500

    Original solutions

---
+++ cars.rb
@@ -0,0 +1,39 @@
+class Car
+	def to_s()
+		"I'm a car! I've driven #{@distance} and have #{@fuel} gallons gas left"
+	end
+
+	def initialize()
+		@fuel = 10
+		@distance = 0
+	end
+
+	def drive(miles)
+		if (@fuel - miles/20.0) >= 0
+			@distance += miles
+			@fuel -= miles/20.0
+		else
+			@distance += @fuel * 20.0
+			@fuel = 0
+			puts "You're out of gas!"
+		end
+	end
+
+	def fuel_up()
+		gallons_needed = 10.0 - @fuel
+		puts "You must pay $#{3.5 * gallons_needed}"
+		@fuel = 10.0
+	end
+end
+
+car_a = Car.new()
+car_b = Car.new()
+puts car_a
+puts car_b
+car_a.drive(10)
+puts car_a
+puts car_b
+car_a.drive(232)
+car_b.drive(117)
+puts car_a
+puts car_b
